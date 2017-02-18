0f5d63f1872b06acd81406141865772cee27c2f8
Alex Nielson <alexnielson90@gmail.com>
2017-02-17 17:53:37 -0800

    (11/29/2016) <Alex Nielson> Added the banner stylization

--- Potterheads/src/byui/cit260/potterheads/view/StartProgramView.java
+++ Potterheads/src/byui/cit260/potterheads/view/StartProgramView.java
@@ -5,7 +5,7 @@
 /*their desired name. The user enters their name and the computer displays a welcome message,
 /*along with a brief description of the game and the Main Menu. An error message is displayed if
 /*the user enters an invalid name or command.
-*/
+ */
 package byui.cit260.potterheads.view;
 
 import byui.cit260.potterheads.control.GameControl;
@@ -17,97 +17,115 @@ import java.util.Scanner;
  * @author Alexandra
  */
 public class StartProgramView {
-   private String promptMessage;
-   
-   public StartProgramView(){
-       this.promptMessage = "\nPlease enter your name: ";
-       // display the banner when view is created
-       this.displayBanner();
-       
-   }
+
+    private String promptMessage;
+
+    public StartProgramView() {
+        this.promptMessage = "\nPlease enter your name: ";
+        // display the banner when view is created
+        this.displayBanner();
+
+    }
 
     private void displayBanner() {
         System.out.println(
-                "\n************************************************************"
-                +"\n* Welcome, Potterheads!                                   *"
-                +"\n* This text-based game takes players throughout           *"
-                +"\n* the world of Harry Potter. Users will discover          *"
-                +"\n* they’re a wizard/witch, obtain school supplies of       *"
-                +"\n* their choice, attend school and learn spells, and       *"
-                +"\n* use these spells to defend and assist themselves as     *"
-                +"\n* they explore the world. As players learn and explore,   *"
-                +"\n* they are awarded “House Points” which are spent on      *"
-                +"\n* mulligans and various items.                            *"
-                +"\n************************************************************"
-                );
-    
+                  "\n_____________________________________________________________________________________________________________________________"        
+                + "\n   ,-.----.                                                                                                                 "
+                + "\n  \\    /  \\                ___      ___                         ,---,                                                     "
+                + "\n  |   :    \\             ,--.'|_  ,--.'|_                     ,--.' |                                ,---,                 "
+                + "\n  |   |  .\\ :   ,---.    |  | :,' |  | :,'             __  ,-.|  |  :                              ,---.'|                 "
+                + "\n  .   :  |: |  '   ,'\\   :  : ' : :  : ' :           ,' ,'/ /|:  :  :                              |   | :  .--.--.        "
+                + "\n  |   |   \\ : /   /   |.;__,'  /.;__,'  /     ,---.  '  | |' |:  |  |,--.   ,---.     ,--.--.      |   | | /  /    '       "
+                + "\n  |   : .   /.   ; ,. :|  |   | |  |   |     /     \\ |  |   ,'|  :  '   |  /     \\   /       \\   ,--.__| ||  :  /`./     "
+                + "\n  ;   | |`-' '   | |: ::__,'| : :__,'| :    /    /  |'  :  /  |  |   /' : /    /  | .--.  .-. | /   ,'   ||  :  ;_          "
+                + "\n  |   | ;    '   | .; :  '  : |__ '  : |__ .    ' / ||  | '   '  :  | | |.    ' / |  \\__\\/: . ..   '  /  | \\  \\    `.   "
+                + "\n  :   ' |    |   :    |  |  | '.'||  | '.'|'   ;   /|;  : |   |  |  ' | :'   ;   /|  ,\" .--.; |'   ; |:  |  `----.   \\    "
+                + "\n  :   : :     \\   \\  /   ;  :    ;;  :    ;'   |  / ||  , ;   |  :  :_:,''   |  / | /  /  ,.  ||   | '/  ' /  /`--'  /    "
+                + "\n  |   | :      `----'    |  ,   / |  ,   / |   :    | ---'    |  | ,'    |   :    |;  :   .'   \\   :    :|'--'.     /      "
+                + "\n  `---'.|                 ---`-'   ---`-'   \\   \\  /          `--''       \\   \\  / |  ,     .-./\\   \\  /    `--'---'  "
+                + "\n    `---`                                    `----'                        `----'   `--`---'     `----'                     "
+                + "\n_____________________________________________________________________________________________________________________________"
+                + "\n" 
+                + "\n"
+                + "\n                                               ***Welcome, Potterheads!***                                                  "
+                + "\n                                       This text-based game takes players throughout"
+                + "\n                                       the world of Harry Potter. Users will discover"
+                + "\n                                       they’re a wizard/witch, obtain school supplies of"
+                + "\n                                       their choice, attend school and learn spells, and"
+                + "\n                                       use these spells to defend and assist themselves as"
+                + "\n                                       they explore the world. As players learn and explore,"
+                + "\n                                       they are awarded “House Points” which are spent on"
+                + "\n                                       mulligans and various items."
+                + "\n*****************************************************************************************************************************"
+        );
+
     }
 
     public void displayStartProgramView() {
-        
+
         boolean done = false; // set flag to not done
         do {
             //prompt for and get playersName
             String playersName = this.getPlayersName();
             if (playersName.toUpperCase().equals("Q")) // user wants to quit
+            {
                 return;
+            }
 
             //do requested action and display next view
             done = this.doAction(playersName);
 
-        //while the view is not done
-
-
+            //while the view is not done
         } while (!done);
-    }    
+    }
 
     private String getPlayersName() {
         Scanner keyboard = new Scanner(System.in); // get infile for keyboard 
-        String value =""; //value to be returned
+        String value = ""; //value to be returned
         boolean valid = false; // initialize to not valid
-        
+
         while (!valid) { // loop while an invalid value is entered
             System.out.println("\n" + this.promptMessage);
-            
+
             value = keyboard.nextLine(); // get next line typed on keyboard
             value = value.trim(); // trim off leading and trailing blanks
-            
-            if (value.length() <1) { //value is blank
+
+            if (value.length() < 1) { //value is blank
                 System.out.println("\nInvalid value: value cannot be blank");
                 continue;
             }
-            
+
             break; // end the loop
         }
-        
+
         return value; // return the value entered
     }
 
     private boolean doAction(String playersName) {
         if (playersName.length() < 2) {
             System.out.println("\nInvalid player name: "
-            + "The name must be greater than one character in length.");
+                    + "The name must be greater than one character in length.");
             return false;
         }
-        
+
         // call createPlayer() control function
         Player player = GameControl.createPlayer(playersName);
-        
+
         if (player == null) { // if unsuccessful
             System.out.println("\nError creating player.");
             return false;
         }
-        
+
         // display next view
         this.displayNextView(player);
         return true;
     }
 
     private void displayNextView(Player player) {
-        System.out.println("Welcome to the Game" + player.getName());
-        
+        System.out.println("\nWelcome to the Game " + player.getName());
+
         MainMenuView mainMenuView = new MainMenuView();
         mainMenuView.displayMainMenuView();
-        
+
     }
 }
